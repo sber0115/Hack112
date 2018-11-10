ae1eda491f48a7e18052e2752b27a88412f63b0d
martinsjohn <44913517+martinsjohn@users.noreply.github.com>
2018-11-10 14:33:58 -0500

    Add files via upload

---
+++ Map.py
@@ -0,0 +1,111 @@
+import module_manager
+module_manager.review()
+
+import pygame
+import Boardmaker
+import Box
+        
+class PygameGame(object):
+
+    def init(self):
+        self.board = Boardmaker.makeBoard(20,Boardmaker.obstacles)
+        print(self.board)
+        self.sizex = self.width // 20
+        self.sizey = self.height // 20
+    def mousePressed(self, x, y):
+        pass
+
+    def mouseReleased(self, x, y):
+        pass
+
+    def mouseMotion(self, x, y):
+        pass
+
+    def mouseDrag(self, x, y):
+        pass
+
+    def keyPressed(self, keyCode, modifier):
+        pass
+
+    def keyReleased(self, keyCode, modifier):
+        pass
+
+    def timerFired(self, dt):
+        pass
+
+    def redrawAll(self, screen):
+        for i in range(20):
+            for j in range(20):
+                X = j* self.sizex
+                Y = i * self.sizey
+                if self.board[i][j] == 1:
+                    obs = Box.Box(X,Y,self.sizex,self.sizey)
+                    Box.Box.draw(obs,screen)
+
+    def isKeyPressed(self, key):
+        ''' return whether a specific key is being held '''
+        return self._keys.get(key, False)
+
+    def __init__(self, width=600, height=600, fps=50, title="112 Pygame Game"):
+        self.width = width
+        self.height = height
+        self.fps = fps
+        self.title = title
+        self.bgColor = (255, 255, 255)
+        pygame.init()
+
+    def run(self):
+
+        clock = pygame.time.Clock()
+        screen = pygame.display.set_mode((self.width, self.height))
+        # set the title of the window
+        pygame.display.set_caption(self.title)
+
+        # stores all the keys currently being held down
+        self._keys = dict()
+
+        # call game-specific initialization
+        self.init()
+        playing = True
+        while playing:
+            time = clock.tick(self.fps)
+            self.timerFired(time)
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    self.mousePressed(*(event.pos))
+                elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    self.mouseReleased(*(event.pos))
+                elif (event.type == pygame.MOUSEMOTION and
+                      event.buttons == (0, 0, 0)):
+                    self.mouseMotion(*(event.pos))
+                elif (event.type == pygame.MOUSEMOTION and
+                      event.buttons[0] == 1):
+                    self.mouseDrag(*(event.pos))
+                elif event.type == pygame.KEYDOWN:
+                    self._keys[event.key] = True
+                    self.keyPressed(event.key, event.mod)
+                elif event.type == pygame.KEYUP:
+                    self._keys[event.key] = False
+                    self.keyReleased(event.key, event.mod)
+                elif event.type == pygame.QUIT:
+                    playing = False
+            screen.fill(self.bgColor)
+            self.redrawAll(screen)
+            pygame.display.flip()
+
+        pygame.quit()
+
+
+def main():
+    game = PygameGame()
+    game.run()
+
+if __name__ == '__main__':
+    main()
+        
+        
+        
+        
+        
+        
+        
